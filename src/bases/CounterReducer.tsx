2cd5712f54501f977166bfbe49e416485d5539d7
Camilo López <camiloj.lopez79@gmail.com>
2023-07-06 13:23:01 -0400

    useReducer

---
+++ src/bases/CounterReducer.tsx
@@ -0,0 +1,33 @@
+// React
+import { useReducer, useState } from 'react';
+
+
+interface CounterState {
+  counter: number;
+  previous: number;
+  changes: number;
+}
+
+const INITIAL_STATE: CounterState = {
+  counter: 0,
+  previous: 0,
+  changes: 0
+};
+
+export const CounterReducer = () => {
+  const [ state, dispatch ] = useReducer( reducer, INITIAL_STATE );
+
+  const handleClick = () => {
+    setCounter( prev => prev + 1 );
+  }
+
+  return (
+    <>
+      <h1>CounterReducer: { counter }</h1>
+
+      <button onClick={ handleClick }>
+        + 1
+      </button>
+    </>
+  );
+}
