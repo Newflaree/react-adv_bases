b9be214b5b56a279e8972e03c3d1383df570e877
Camilo López <camiloj.lopez79@gmail.com>
2023-07-06 11:00:51 -0400

    CustomHooks con referencias HTML

---
+++ src/bases/CounterHook.tsx
@@ -0,0 +1,19 @@
+import { useCounter } from '../hooks';
+
+const MAXIMUN_COUNT = 10;
+
+export const CounterHook = () => {
+  const { counter, counterElement, handleClick } = useCounter();
+
+
+  return (
+    <>
+      <h1>CounterHook:</h1>
+      <h2 ref={ counterElement }>{ counter }</h2>
+
+      <button onClick={ handleClick }>
+        + 1
+      </button>
+    </>
+  );
+}
