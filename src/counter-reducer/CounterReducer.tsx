636c9011924f2d49edf68ed58e3e4ea7b54c310a
Camilo López <camiloj.lopez79@gmail.com>
2023-07-13 17:03:05 -0400

    Separar acciones, interfaces y reducer

---
+++ src/counter-reducer/CounterReducer.tsx
@@ -0,0 +1,45 @@
+// React
+import { useReducer, useState } from 'react';
+// Interfaces
+import { CounterState } from './interfaces';
+// State
+import { counterReducer } from './state';
+
+
+const INITIAL_STATE: CounterState = {
+  counter: 0,
+  previous: 0,
+  changes: 0
+};
+
+export const CounterReducer = () => {
+  const [ counterState, dispatch ] = useReducer( counterReducer, INITIAL_STATE );
+
+  const handleReset = () => {
+    dispatch({ type: 'reset' })
+  }
+
+  const handleIncreaseBy = ( value: number ) => {
+    dispatch({ type: 'increaseBy', payload: { value } });
+  }
+
+  return (
+    <>
+      <h1>Counter Reducer Segmentado</h1>
+      <pre>{ JSON.stringify( counterState, null, 2 ) }</pre>
+
+      <button onClick={ () => handleIncreaseBy( 1 ) }>
+        +1 
+      </button>
+      <button onClick={ () => handleIncreaseBy( 5 ) }>
+        +5
+      </button>
+      <button onClick={ () => handleIncreaseBy( 10 ) }>
+        +10
+      </button>
+      <button onClick={ handleReset }>
+        reset
+      </button>
+    </>
+  );
+}
